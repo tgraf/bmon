c80554be16f51b908229ed345df81b760ffd4dd4
Tobias Klauser <tklauser@distanz.ch>
2015-04-30 11:27:24 +0200

    Explicitly include linux/if.h to fix cross-compile error
    When compiling bmon with the Linaro 2014.01 ARM toolchain
    (gcc-linaro-arm-linux-gnueabihf-4.8-2014.01_linux) the following compile error occurs:
    
      in_netlink.c: In function ‘do_link’:
      in_netlink.c:688:53: error: ‘IFF_UP’ undeclared (first use in this function)
    
    Fix it by explicitly including linux/if.h, where IFF_UP is defined.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- src/in_netlink.c
+++ src/in_netlink.c
@@ -37,6 +37,8 @@ static int c_notc = 0;
 static struct element_group *grp;
 static struct bmon_module netlink_ops;
 
+#include <linux/if.h>
+
 #include <netlink/netlink.h>
 #include <netlink/cache.h>
 #include <netlink/utils.h>
